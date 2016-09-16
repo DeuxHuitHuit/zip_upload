bb619852454d2dac4e8bd48b5c5349069c177527
Nicolas Brassard <nico@nitriques.com>
2016-09-16 18:24:57 -0400

    delete + update logic

--- fields/field.zip_upload.php
+++ fields/field.zip_upload.php
@@ -53,24 +53,90 @@
 		public function processRawFieldData($data, &$status, &$message = null, $simulate = false, $entry_id = null)
 		{
 			$ret = parent::processRawFieldData($data, $status, $message, $simulate, $entry_id);
-			// new upload
-			if ($status === self::__OK__ && is_array($data) && !empty($data['tmp_name']) && $data['error'] === UPLOAD_ERR_OK) {
-				$root = DOCROOT . trim($this->get('destination'), '') . '/';
-				$file = $root . $ret['file'];
-				$dest = $root . basename($ret['file'], '.zip');
-				if (@file_exists($dest)) {
-					$message = __("Destination folder `%s` already exists.", array($dest));
-					$status = self::__ERROR_CUSTOM__;
-					return $ret;
+			if ($simulate) {
+				return $ret;
+			}
+			
+			// parent went well
+			if (is_array($data) && $status === self::__OK__) {
+				// Get our locations
+				$locations = $this->getFilesLocations($ret);
+				$file = $locations['file'];
+				$dest = $locations['dest'];
+				$existing_file = null;
+				
+				// Check to see if the entry already has a file associated with it:
+				if (is_null($entry_id) === false) {
+					$row = Symphony::Database()->fetchRow(0, sprintf(
+						"SELECT *
+						FROM `tbl_entries_data_%s`
+						WHERE `entry_id` = %d
+						LIMIT 1",
+						$this->get('id'),
+						$entry_id
+					));
+
+					$existing_file = isset($row['file']) ? $this->getFilePath($row['file']) : null;
+					$exitingLocations = $this->getFilesLocations(array('file' => $existing_file));
+
+					// File was removed:
+					if (
+						$data['error'] == UPLOAD_ERR_NO_FILE
+						&& !is_null($existing_file)
+						&& is_dir($exitingLocations['dest'])
+					) {
+						General::deleteDirectory($exitingLocations['dest']);
+					}
 				}
-				General::realiseDirectory($dest, Symphony::Configuration()->get('write_mode', 'directory'));
-				if (!$this->unzipFile($dest, $file)) {
-					$message = __("Failed to unzip `%s`.", array(basename($file)));
-					$status = self::__ERROR_CUSTOM__;
-					return $ret;
+				
+				// new upload
+				if (is_array($data) && !empty($data['tmp_name']) && $data['error'] === UPLOAD_ERR_OK) {
+					if (@file_exists($dest)) {
+						$message = __("Destination folder `%s` already exists.", array($dest));
+						$status = self::__ERROR_CUSTOM__;
+						return $ret;
+					}
+					General::realiseDirectory($dest, Symphony::Configuration()->get('write_mode', 'directory'));
+					if (!$this->unzipFile($dest, $file)) {
+						$message = __("Failed to unzip `%s`.", array(basename($file)));
+						$status = self::__ERROR_CUSTOM__;
+						return $ret;
+					}
+				}
+				
+				// File has been replaced:
+				if (
+					isset($existing_file)
+					&& $existing_file !== $file
+					&& is_dir($exitingLocations['dest'])
+				) {
+					General::deleteDirectory($exitingLocations['dest']);
 				}
 			}
 			return $ret;
+		}
+		
+		/*-------------------------------------------------------------------------
+			Utilities:
+		-------------------------------------------------------------------------*/
+
+		public function getFilesLocations($ret)
+		{
+			$root = DOCROOT . trim($this->get('destination'), '') . '/';
+			return array(
+				'file' => $root . $ret['file'],
+				'dest' => $root . basename($ret['file'], '.zip'),
+			);
+		}
+
+		public function entryDataCleanup($entry_id, $data = null)
+		{
+			parent::entryDataCleanup($entry_id, $data);
+			if (is_array($data)) {
+				$locations = $this->getFilesLocations($data);
+				General::deleteDirectory($locations['dest']);
+			}
+			return true;
 		}
 		
 		private function unzipFile($dest, $file)
